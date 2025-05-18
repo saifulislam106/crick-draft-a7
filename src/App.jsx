1ec59e1c4ada7884af694d7486b75d10a87e6622
saifulislam106 <saifulislam106915@gmail.com>
2025-05-18 14:40:23 +0600

    added player card

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Footer from './components/footer/Footer'
 import { Navbar } from './components/navbar/Navbar'
+import PlayerCards from './components/playerCards/PlayerCards'
 
 function App() {
 
@@ -9,7 +10,9 @@ function App() {
   return (
     <>
      <Navbar />
-<div className='min-h-screen'></div>
+<div className='min-h-screen container mx-auto'>
+  <PlayerCards />
+</div>
      <Footer />
   
     </>
---
+++ src/components/playerCards/PlayerCards.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+
+const PlayerCards = () => {
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    fetch("/public/fack.json")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setPlayers(data);
+      });
+  }, []);
+  return (
+    <div>
+      <h1>Player : {players.length}</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {players.map((player, idx) => (
+          <div key={idx} className="card w-96 bg-base-100 shadow-xl">
+            <figure>
+              <img className="w-full" src={player.img} alt={player.name} />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{player.name}</h2>
+              <div className="flex justify-between">
+                <div className="flex items-center gap-2">
+                  <figure>
+                    <img
+                      className="w-8"
+                      src={player.country_flag}
+                      alt={player.name}
+                    />
+                  </figure>
+                  <p className="text-sm"> {player.country}</p>
+                </div>
+                <div>
+                  <p className="bg-gray-200 rounded-lg p-2">
+                    {player.player_type}
+                  </p>
+                </div>
+              </div>
+              <div className="border-t-2 border-gray-200 pt-4 mt-2">
+                <p className="font-bold">{player.batting_style}</p>
+                <p className="font-bold">{player.bowling_style}</p>
+                <div className="flex justify-between items-center">
+                  <p className="font-bold">Price: {player.price}</p>
+                  <button className="btn ">Player Choose</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PlayerCards;
